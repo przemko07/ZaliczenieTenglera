a0276204da4fcbe977a229a4e5c23ff4fd167d44
Przemko07 <theprzemko07@gmail.com>
2014-06-10 23:25:13 +0200

    ocenianie firmy skońćzone

--- PorownywarkaFirm/gui/ViewModels/OcenaFirmyVM.cs
+++ PorownywarkaFirm/gui/ViewModels/OcenaFirmyVM.cs
@@ -1,22 +1,52 @@
 ﻿using Logika;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
 namespace gui.ViewModels
 {
+    public class OcenaCzastkowa : DisplayNameAttribute
+    {
+        public OcenaCzastkowa()
+            : base()
+        {
+        }
+        public OcenaCzastkowa(string name)
+            : base(name)
+        {
+        }
+    }
     public class OcenaFirmyVM : FirmaVM
     {
+        [OcenaCzastkowa("Wygląd firmy")]
         public int wyglad_firmy { get; set; }
+
+        [OcenaCzastkowa("Poziom obsługi")]
         public int poziom_obslugi { get; set; }
+
+        [OcenaCzastkowa("Czas usług")]
         public int czas_swiadczenia_uslug { get; set; }
+
+        [OcenaCzastkowa("Lokalizacja")]
         public int lokalizacja { get; set; }
+
+        [OcenaCzastkowa("Poziom usług")]
         public int poziom_swiadczonej_uslugi { get; set; }
+
+        [OcenaCzastkowa("Atmosfera")]
         public int atmosera { get; set; }
+
+        [OcenaCzastkowa("Zarobki")]
         public int zarobki { get; set; }
+
+        [OcenaCzastkowa("Kontakt z przełożonym")]
         public int kontakt_z_przelozonymi { get; set; }
+
+        [OcenaCzastkowa("Wyposażenie")]
         public int wyposazenie { get; set; }
 
         public OcenaFirmyVM(Firma firma)
